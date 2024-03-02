bb7c15ed93cbcdca7c2c6497caf11c6790f4e372
jordon <jordon.programming@gmail.com>
2024-03-02 15:00:42 +0900

    쿼리DSL, JPA 설정 완료

--- src/main/java/brew/BrewApplication.java
+++ src/main/java/brew/BrewApplication.java
@@ -1,13 +1,52 @@
 package brew;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BrewApplication {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BrewApplication.class, args);
     }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        // hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
+
+    @Bean
+    AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
