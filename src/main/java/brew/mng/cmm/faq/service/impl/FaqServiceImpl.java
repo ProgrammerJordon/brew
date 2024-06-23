156a342956e701f20dc8b255cca3897379c326c6
jordon <jordon.programming@gmail.com>
2024-06-24 01:33:14 +0900

    brew sample 코드 위치 변경 및 원복

--- src/main/java/brew/mng/cmm/faq/service/impl/FaqServiceImpl.java
+++ src/main/java/brew/mng/cmm/faq/service/impl/FaqServiceImpl.java
@@ -1,12 +1,12 @@
 package brew.mng.cmm.faq.service.impl;
 
-import brew.mng.cmm.bbs.service.impl.BoardDAO;
+import brew.mng.cmm.faq.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service("FaqService")
 @RequiredArgsConstructor
-public class FaqServiceImpl {
+public class FaqServiceImpl implements FaqService {
 
     private final FaqDAO faqDAO;
 
---
+++ src/main/java/brew/mng/cmm/spl/web/SampleController.java
@@ -0,0 +1,16 @@
+package brew.cmm.sample.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mng/cmm/spl")
+@RequiredArgsConstructor
+public class SampleController {
+
+    @RequestMapping("/selectSampleListVw.do")
+    public String selectSampleListVw() {
+        return "/mng/cmm/spl/selectSampleListVw";
+    }
+}
