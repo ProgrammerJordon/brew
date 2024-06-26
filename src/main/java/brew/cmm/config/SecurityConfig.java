03f917ebf9fb53e74d87764f60ca9e7ecd68cb36
jordon <jordon.programming@gmail.com>
2024-06-26 21:05:00 +0900

    brew 스프링시큐리티 추가

---
+++ src/main/java/brew/cmm/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package brew.cmm.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll() // 모든 경로에 대한 접근 허용
+                )
+                .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
+        return http.build();
+    }
+}
