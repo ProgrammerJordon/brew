db152b131f0f3e9448f9c7c3819e9cd0aa9f6fd3
Beomsu Jeong <jordon.programming@gmail.com>
2024-07-09 09:21:00 +0900

    MessageUtil 주입방식 및 aware implements 제거

--- src/main/java/brew/cmm/exception/BrewApplicationException.java
+++ src/main/java/brew/cmm/exception/BrewApplicationException.java
@@ -3,45 +3,32 @@ package brew.cmm.exception;
 import brew.cmm.service.ppt.BrewProperties;
 import brew.cmm.util.BrewMessageUtil;
 import lombok.Getter;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @SuppressWarnings("serial")
-public class BrewApplicationException extends RuntimeException implements ApplicationContextAware {
-
-    private static ApplicationContext context;
+public class BrewApplicationException extends RuntimeException {
 
     private HttpStatus httpStatusCode;
     private String message;
 
-    public BrewApplicationException() {
+    public BrewApplicationException(BrewMessageUtil brewMessageUtil) {
         this.httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        this.message = getBrewMessageUtil().getMessage(BrewProperties.getProperty("fail.common.msg"));
+        this.message = brewMessageUtil.getMessage(BrewProperties.getProperty("fail.common.msg"));
     }
 
-    public BrewApplicationException(String message) {
+    public BrewApplicationException(String message, BrewMessageUtil brewMessageUtil) {
         this.httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        this.message = getBrewMessageUtil().getMessage(message);
+        this.message = brewMessageUtil.getMessage(message);
     }
 
-    public BrewApplicationException(String messageCode, Object[] args) {
+    public BrewApplicationException(String messageCode, Object[] args, BrewMessageUtil brewMessageUtil) {
         this.httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        this.message = getBrewMessageUtil().getMessage(messageCode, args);
+        this.message = brewMessageUtil.getMessage(messageCode, args);
     }
 
     public BrewApplicationException(String message, HttpStatus httpStatusCode) {
         this.httpStatusCode = httpStatusCode;
         this.message = message;
-    }
-
-    private BrewMessageUtil getBrewMessageUtil() {
-        return context.getBean(BrewMessageUtil.class);
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        context = applicationContext;
     }
 }
