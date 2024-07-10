6b3793ff12910871613ef05cca5e11a0c79a4ff8
Beomsu Jeong <jordon.programming@gmail.com>
2024-07-10 09:59:44 +0900

    @EqualsAndHashCode(callSuper=false) 추가

--- src/main/java/brew/mng/dtm/cam/service/CollectApiVO.java
+++ src/main/java/brew/mng/dtm/cam/service/CollectApiVO.java
@@ -2,10 +2,12 @@ package brew.mng.dtm.cam.service;
 
 import brew.cmm.vo.CommonVO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class CollectApiVO  extends CommonVO implements Serializable {
 
     private String method;
