d87880b7b47aecd692a3f90d9a48071e7e62431e
younseonglyong <zx2935@gmail.com>
2024-08-12 22:42:14 +0900

    PersonalRankingVO 뭐 넣을지 생각하고 테이블 구성해서 만들자
    에러수정

--- src/main/java/brew/svc/csc/psr/service/Impl/PersonalRankingDAO.java
+++ src/main/java/brew/svc/csc/psr/service/Impl/PersonalRankingDAO.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository("NoticeDAO")
+@Repository("PersonalRankingDAO")
 @RequiredArgsConstructor
 public class PersonalRankingDAO {
 
